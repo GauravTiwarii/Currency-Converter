4d4428648d8a549207f56a7244fd1a7b036a1ec2
Love <Love@macbooks-macbook-pro.local>
2016-06-02 02:07:18 +0530

    Initial Commit

---
+++ Currency Converter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Currency Converter
+//
+//  Created by Love on 6/2/16.
+//  Copyright © 2016 Love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
